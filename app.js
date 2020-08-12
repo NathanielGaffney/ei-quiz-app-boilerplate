fd9330e4e3445b00578d4f049ada36b7725f6fe5
NathanielGaffney <nate440@gmail.com>
2020-08-11 20:09:31 -0400

    beginning js and html insertion via jquery

--- app.js
+++ app.js
@@ -57,12 +57,15 @@ let score = 0;
 let questionNumber = 0;
 
 
-function generateStartScreen{
-// button that triggers start quiz
-// html image tag location
+function generateStartScreen(){
+  return(`<div class='primary' style="background-image: url('images/title.jpeg')">
+  <button class='start'>
+    Start The Quiz!
+  </button>
+</div>`);
 }
 
-function generateQuestionScreen{
+function generateQuestionScreen(){
 // html image tag location
 // display question form based on questionNumber
 // display score and question count
@@ -70,13 +73,13 @@ function generateQuestionScreen{
 // shows updated question counter 
 }
 
-function generateNextQuestionScreen{
+function generateNextQuestionScreen(){
 // Displays results from previous
 // shows updated score counter
 // displays "next" question button
 }
 
-function generateEndScreen{
+function generateEndScreen(){
 // html image tag location
 // congrats message from array, based on score
 // score
@@ -89,32 +92,34 @@ function generateEndScreen{
 // if the questionNumber is 0 then generateStartScreen
 // if the questionNumber is 1-5 generateQuestion screen
 // if the questionNumber is 6 generateEndScreen
-}
+//}
 
 ////////////
 
 function render(arg){
  // varible is  = arg, which could be generateScreen() or generateNextQuestionScreen;
  // jquery location .html(variable)
+ let x = arg;
+  $('main').html(x);
 }
 
 /////////
 
 // We could potentially pass in all other generate functions as an argument (arg) in our render function
 
-function handleStart{
+function handleStart(){
 // jquery will point to our start button and will listen for a click
 // on click:
 //  increment questionNumber counter
 //  call render function
 }
 
-function checkAnswer{
+function checkAnswer(){
 // this will check if the answer
 // add to our score acc
 }
 
-function handleSubmit{
+function handleSubmit(){
 // jquery will point to our start button and will listen for a click
 // on click:
 //  run generate nextQuestionScreen
@@ -123,14 +128,14 @@ function handleSubmit{
 
 }
 
-function handleNext{
+function handleNext(){
 // jquery will point to our start button and will listen for a click
 // on click:
 //  increment questionNumber counter
 //  call render function with generateNextQUestionScreen as arg;
 }
 
-function handleRestart{
+function handleRestart(){
 // jquery will point to our start button and will listen for a click
 // on click:
 //  call render function with generateStartScreen as arg
@@ -138,12 +143,12 @@ function handleRestart{
 //  reset score counter
 }
 
-function eventHandler{
+function eventHandler(){
 // render
 // all handles
 }
 
-
+//$(render(generateStartScreen));
 
 
 
@@ -160,7 +165,7 @@ function eventHandler{
  */
 //const store = {
   // 5 or more questions are required
-  questions: [
+ /* questions: [
     {
       question: 'What color is broccoli?',
       answers: [
@@ -185,7 +190,7 @@ function eventHandler{
   //quizStarted: false,
   //questionNumber: 0,
   //score: 0
-};
+};*/
 
 /**
  * 
